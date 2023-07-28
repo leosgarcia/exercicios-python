9d8f2239215b37549e8b469e702b2aab943bc154
Leonardo Garcia <leosgarcia@gmail.com>
2023-07-28 18:47:11 -0300

    Commit ex003

---
+++ ex003/ex003.py
@@ -0,0 +1,21 @@
+n1 = float(input('Digite um número: '))
+n2 = float(input('Digite outro número: '))
+
+soma = n1 + n2
+subtracao = n1 - n2
+multiplicacao = n1 * n2
+divisao = n1 / n2
+divisao_inteira = n1 // n2
+divisao_resto = n1 % n2
+potencia = n1**n2 
+
+print(f'A soma de {n1} e {n2} é {soma}')
+print(f'A subtração de {n1} e {n2} é {subtracao}')
+print(f'A multiplicação de {n1} e {n2} é {multiplicacao}')
+print(f'A divisão de {n1} e {n2} é {divisao}')
+
+
+
+print(divisao_inteira)
+print(divisao_resto)
+print(potencia5)
