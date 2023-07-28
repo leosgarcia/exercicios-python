cefc031c8c17c5acc6c1838b1925f7b76236ff94
Leonardo Garcia <leosgarcia@gmail.com>
2023-07-28 17:42:22 -0300

    Ajuste tabuada

--- tabuada/taboada.py
+++ tabuada/taboada.py
@@ -4,5 +4,5 @@ for tab in range(1, 11):
     print("TABUADA DE {}".format(tab))
     print("--------------")
     for num in range(1, 11):
-        print("|{0:2d} X {0:2d} = {0:2d}|".format(num, tab, num * tab))
+        print("|{0:2d} X {1:2d} = {2:2d}|".format(num, tab, num * tab))
     print("--------------\n")
