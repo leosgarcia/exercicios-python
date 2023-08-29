9f4ec518ad1bbaba7661fa36c837cd7aa563d1e0
Leonardo Garcia <leosgarcia@gmail.com>
2023-08-29 15:55:37 -0300

    Add equação do segundo grau

--- logica-de-programacao/equacao2nd.py
+++ logica-de-programacao/equacao2nd.py
@@ -1,7 +1,13 @@
+from math import sqrt
+
 a = float(input('Digite o valor de A: '))
 b = float(input('Digite o valor de B: '))
 c = float(input('Digite o valor de C: '))
 
-delta = ((b ** 2) - (4 * a * c)) / (2 * a)
+delta = ((b ** 2) - (4 * a * c))
+x1 = (-b + sqrt(delta)) / (2 * a)
+x2 = (-b - sqrt(delta)) / (2 * a)
 
-print(delta)
+print('O valor de delta é: {}'.format(delta))
+print('O valor de x1 é: {}'.format(x1))
+print('O valor de x2 é: {}'.format(x2))
